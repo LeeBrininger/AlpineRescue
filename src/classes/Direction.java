58908765547deba229a5d0053b7b5a28ec61e0f6
Sam Markoe <somarkoe@gmail.com>
2014-04-26 23:01:33 -0600

    Added searcher config file

--- src/classes/Direction.java
+++ src/classes/Direction.java
@@ -1,5 +1,9 @@
 package classes;
 
 public enum Direction {
-	NORTHEAST, NORTH, NORTHWEST, WEST, SOUTHWEST, SOUTH, SOUTHEAST, EAST
+	NORTHEAST(-1, 1), NORTH(-1,0), NORTHWEST(-1,-1), WEST(0,-1), SOUTHWEST(1,-1), SOUTH(1,0), SOUTHEAST(1,1), EAST(0,1);
+	
+	private Direction(int row, int column) {
+		
+	}
 }
